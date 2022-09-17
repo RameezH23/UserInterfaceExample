12832274d80b14869acbe359f8950fffb1fc0fb7
Rameez Hanif <78284285+RameezH23@users.noreply.github.com>
2022-09-16 21:15:53 -0500

    PICKER (fr this time)

--- UserInterfaceExample/FrontViewController.swift
+++ UserInterfaceExample/FrontViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class FrontViewController: UIViewController {
+class FrontViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var selectionBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pickerView.delegate=self
+        pickerView.dataSource=self
     }
     
-
+//    @IBAction func selectPressed(_ sender: UIButton) {
+//        
+//    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ["1","2","3","4","5","6","7","8","9","10"][row]
+    }
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//    }
 }
