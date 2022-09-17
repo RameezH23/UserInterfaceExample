e094d95bb1ec101169544ff5d3710ff007fbdbbd
Diogo Benetti Faé Rodrigues <diogo.fae2001@gmail.com>
2022-09-16 21:46:16 -0500

    Text field and slider working

--- UserInterfaceExample/FrontViewController.swift
+++ UserInterfaceExample/FrontViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class FrontViewController: UIViewController {
+class FrontViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var recValue: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var nameView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.nameField.delegate = self
+        
+        slider.value = 0
+        recValue.text = String(Int(slider.value))
 
         // Do any additional setup after loading the view.
     }
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        var name = self.nameField.text
+//        var hello_string = "Hello, " + name!
+        self.nameView.text = "Hello, " + self.nameField.text!
+        return self.nameField.resignFirstResponder()
+    }
+    
+    @IBAction func changeRecVal(_ sender: Any) {
+        slider.maximumValue = 10
+        slider.minimumValue = 0
+        recValue.text = String(Int(slider.value))
+    }
+    
 }
